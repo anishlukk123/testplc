1586248f0de8e333425e1b6b12bdc58d1c02885e
Anish Lukkireddy <57578712+anishlukk123@users.noreply.github.com>
2023-05-01 15:43:55 -0400

    Add files via upload

---
+++ test2/src/test2/Loxcallable.java
@@ -0,0 +1,11 @@
+package test2;
+
+
+import java.util.List;
+
+interface LoxCallable {
+//> callable-arity
+  int arity();
+//< callable-arity
+  Object call(Interpreter interpreter, List<Object> arguments);
+}
---
+++ test2/src/test2/TokenType.java
@@ -0,0 +1,24 @@
+package test2;
+
+enum TokenType {
+	
+	
+	  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+	  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+	  // One or two character tokens.
+	  BANG, BANG_EQUAL,
+	  EQUAL, EQUAL_EQUAL,
+	  GREATER, GREATER_EQUAL,
+	  LESS, LESS_EQUAL,
+
+	  // Literals.
+	  IDENTIFIER, STRING, NUMBER,
+
+	  // Keywords.
+	  ANDA, CLASS, ELSEE, FALSE, FUN, FORA, IFA, NIL, ORA,
+	  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHIL,
+
+	  EOF, DO
+
+}
